5cc3dc7b7b6b5ba9a23699d71ce885afcc963b60
kazunoki <kazukingh01@gmail.com>
2021-11-10 12:19:50 +0900

    modify

--- build/lib/kkdetectron2/__init__.py
+++
@@ -1,4 +0,0 @@
-from kkdetectron2 import detector
-from kkdetectron2 import util
-from kkdetectron2 import mapper
-from kkdetectron2 import bytetrack
--- build/lib/kkdetectron2/util/__init__.py
+++
@@ -1,3 +0,0 @@
-from kkdetectron2.util import com
-from kkdetectron2.util import image
-from kkdetectron2.util import logger
